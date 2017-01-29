9f0530338340e63e57f4ae70cd5ef2cc30004373
pierrepas <pierrepas@yahoo.fr>
2017-01-29 21:23:48 +0100

    Ajout de la DB

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
+
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import Chose
+
+# set up automated slug creation
+class ChoseAdmin(admin.ModelAdmin):
+    model = Chose
+    list_display = ('nom', 'description',)
+    prepopulated_fields = {'slug': ('nom',)}
+
+# and register it
+admin.site.register(Chose, ChoseAdmin)
--- collection/models.py
+++ collection/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Thing(models.Model):
-    name = models.CharField(max_length=255)
+class Chose(models.Model):
+    nom = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
